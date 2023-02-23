3cf3edf75a8e9d5f6876cecff82e0bfc1970a19f
Brian Kariuki <mwangi.ict@gmail.com>
2023-02-22 18:19:41 -0800

    Updating

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,32 +1,11 @@
 #ifndef main_h
-
 #define main_h
 
-extern int _putchar(char c);
+#include <stdio.h>
 
-extern void print_alphabet(void);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-extern void print_alphabet_x10(void);
-
-extern int _islower(int c);
-
-extern int _isalpha(int c);
-
-extern int print_sign(int n);
-
-extern int _abs(int);
-
-extern int print_last_digit(int);
-
-extern void jack_bauer(void);
-
-extern void times_table(void);
-
-extern int add(int, int);
-
-extern void print_to_98(int n);
-
-extern void print_times_table(int n);
-
-#endif
-
+#endif /* main_h*/
